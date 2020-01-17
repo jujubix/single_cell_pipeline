f01352e5dc0b583a602108a07c53bca872ccc074
Douglas Abrams <abramsd@mskcc.org>
2020-01-17 11:37:06 -0500

    jenkins testing: alingment, annotation, hmmcopy, merge/split bam, variant/breakpoint calling. code written/untested for infer/count haps.

--- tests/jenkins/merge_cell_bams/merge_cell_bams.sh
+++ tests/jenkins/merge_cell_bams/merge_cell_bams.sh
@@ -2,12 +2,10 @@
 
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
-
-mkdir -p MERGE_CELL_BAMS/bams/
+mkdir -p MERGE_CELL_BAMS/ref_test_data
 
 docker run -v $PWD:$PWD -w $PWD singlecellpipeline/azurecli:v0.0.1 \
-  az storage blob download-batch -s merge-bams -d MERGE_CELL_BAMS/bams/ --account-name $1 --account-key $2
-
+  az storage blob download-batch -s merge-bams -d MERGE_CELL_BAMS/ref_test_data --account-name $1 --account-key $2
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
@@ -18,3 +16,10 @@ docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/va
   --submit local --loglevel DEBUG \
   --tmpdir MERGE_CELL_BAMS/temp \
   --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
+
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker --rm \
+  singlecellpipeline/single_cell_pipeline:$TAG \
+  python tests/jenkins/merge_cell_bams/test_merge_cell_bams.py MERGE_CELL_BAMS/output  MERGE_CELL_BAMS/ref_test_data/refdata
+
+#rm -r MERGE_CELL_BAMS
