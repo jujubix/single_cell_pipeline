1ede4905ec771570b329c07a9f955f7af58749d9
Diljot Grewal <diljot.grewal@gmail.com>
2020-01-20 13:39:06 -0500

    minor

--- tests/jenkins/count_haps/count_haps.sh
+++ tests/jenkins/count_haps/count_haps.sh
@@ -17,4 +17,5 @@ docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/va
   --context_config tests/jenkins/count_haps/context_config.yaml \
   --submit local --loglevel DEBUG \
   --tmpdir COUNT_HAPS/temp \
+  --config_override '{"infer_haps":{"chromosomes":["15"], "extract_seqdata": {"genome_fai_template": "/refdata/human/infer_haps/GRCh37-lite.fa.fai", "genome_fasta_template": "/refdata/human/infer_haps/GRCh37-lite.fa"}, "ref_data_dir": "/refdata/human/infer_haps/"}}' \
   --pipelinedir COUNT_HAPS/pipeline --submit local --out_dir COUNT_HAPS/output
